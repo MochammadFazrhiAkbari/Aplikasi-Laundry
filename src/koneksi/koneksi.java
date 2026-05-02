6f62a7fd196531f2cfec7baf50d863be14ed8a67
MochammadFazhriAkbari <fazhriakbari48@gmail.com>
2026-05-02 11:36:19 +0700

    Ini adalah Aplikasi Laundry berbasis Java

---
+++ src/koneksi/koneksi.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Hype
+ */
+public class koneksi {
+   private static Connection mysqlkoneksi;
+    public static Connection getKoneksi() { // Pastikan 'public static'
+        if (mysqlkoneksi == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/laundry";
+                String user = "root";
+                String pass = "";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                mysqlkoneksi = DriverManager.getConnection(url, user, pass);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Koneksi Gagal: " + e.getMessage());
+            }
+        }
+        return mysqlkoneksi;
+    }
+}
